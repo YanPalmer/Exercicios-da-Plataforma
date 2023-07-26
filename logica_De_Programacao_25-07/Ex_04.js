4f0bf5c351f7cabf90c998526b483ac67b273d7b
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-25 21:43:31 -0300

    Ex04 Concluído

--- logica_De_Programacao_25-07/Ex_04.js
+++ logica_De_Programacao_25-07/Ex_04.js
@@ -1,1 +1,17 @@
 // Crie um programa que receba a idade de uma pessoa e informa se ela é maior ou menor de idade.
+
+const prompt = require("prompt-sync")();
+verificarIdade();
+function verificarIdade() {
+    let idade = Number(prompt("Digite a sua idade: "));
+    if (idade < 18) {
+        console.log("Você é menor de idade. Cuidado!");
+    } else if (idade >= 18) {
+        console.log("Você já tem idade de saber o que é certo e errado. Previna-se!");
+    } else {
+        console.log("\nVocê digitou algum número?\n Tente novamente...");
+        return verificarIdade();
+    };
+}
+
+// OK
