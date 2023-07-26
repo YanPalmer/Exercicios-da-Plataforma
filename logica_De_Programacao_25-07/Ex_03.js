4262d9f9ce3d22edc337ca32e2289a2b35afcdf8
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-25 21:27:04 -0300

    Ex03 Concluído

--- logica_De_Programacao_25-07/Ex_03.js
+++ logica_De_Programacao_25-07/Ex_03.js
@@ -1,1 +1,12 @@
 // Escreva um programa que calcule a soma de dois números inteiros e exiba o resultado.
+
+const prompt = require("prompt-sync")();
+
+console.log("Programa para somar dois valores...");
+let n1 = Number(prompt("Digite o primeiro número: "));
+let n2 = Number(prompt("Digite o segundo número: "));
+let resultado = n1 + n2;
+
+console.log(`A soma entre ${n1} e ${n2} é: ${resultado}`);
+
+// OK
