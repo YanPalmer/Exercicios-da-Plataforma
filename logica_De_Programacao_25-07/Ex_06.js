4af7b55901fd48574bc0d0deae9742d560a9cfdf
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-25 23:07:20 -0300

    Ex06 Concluído

--- logica_De_Programacao_25-07/Ex_06.js
+++ logica_De_Programacao_25-07/Ex_06.js
@@ -1,1 +1,12 @@
 // Escreva um programa que calcule a média de três números informados pelo usuário.
+
+const prompt = require("prompt-sync")();
+console.log("Programa para calcular a média de três números...");
+let n1 = Number(prompt("Digite o primeiro número: "));
+let n2 = Number(prompt("Digite o segundo número: "));
+let n3 = Number(prompt("Digite o terceiro número: "));
+let media = (n1 + n2 + n3) / 3;
+
+console.log(`A média dos números ${n1}, ${n2} e ${n3} é: ${media}`);
+
+// OK
