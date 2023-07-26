ea9af8a6f886e37badd73f1670d541a232414735
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-25 21:22:08 -0300

    Ex02 Concluído

--- logica_De_Programacao_25-07/Ex_02.js
+++ logica_De_Programacao_25-07/Ex_02.js
@@ -1,1 +1,7 @@
 // Crie um programa que solicite o nome do usuário e, em seguida, imprima uma mensagem personalizada com o nome inserido.
+
+const prompt = require("prompt-sync")();
+var nome = prompt("Digite o seu nome: ");
+console.log(`Olá ${nome}! Seja bem vindo ao mundo.`)
+
+// OK
